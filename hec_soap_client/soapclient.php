42093f2d216d8c6009260051ea84c0951f58ea27
Umair Anwar <umair.anwar@jadootv.com>
2015-07-12 20:57:44 +0500

    1) WSDL Based Soap Client 2) Fault-based test approach for xml

--- hec_soap_client/soapclient.php
+++ hec_soap_client/soapclient.php
@@ -1,9 +1,27 @@
 <?php
 //$options = array("location" => "http://203.99.61.82/umair/thesis/uni_soap_server/soapserver.php", "uri" => "http://localhost");
-$options = array("location" => "http://localhost/thesis/uni_soap_server/soapserver.php", "uri" => "http://localhost");
+//$options = array("location" => "http://localhost/thesis/uni_soap_server/soapserver.php", "uri" => "http://localhost");
+//$options = array("location" => "http://10.66.100.111/dressclient/dress-master/uni_soap_server/soapserver.php", "uri" => "http://localhost");
+
+//$options = array("location" => "http://10.66.100.111/dressclient/dress-master/uni_soap_server/soapserver.php", "uri" => "http://localhost");
+//http://10.66.100.111/dressclient/dress-master/uni_soap_server/gen_wsdl.php/
+
 try {
-	$client = new SoapClient(null, $options);
+	//$client = new SoapClient(null, $options);
+	
+	// Using WSDL Mode, Second parameter $options is optional in this mode.
+	$client = new SoapClient("http://10.66.100.111/dressclient/dress-master/uni_soap_server/gen_wsdl.php/"); 
+	
 	//$student = $client->makeAgreement();
+	
+	//print_r($client->__getFunctions());
+	//print_r($client);
+	//print_r($client->__getTypes());
+	$student = $client->validateDegree(1);
+	//print_r($student);
+	//exit;
+	
+	//print_r($client->__getFunctions());
 	
 	if (isset($_GET['operation']) && $_GET['operation'] == 'findStudent') {
 		$student = $client->findStudent(1);
